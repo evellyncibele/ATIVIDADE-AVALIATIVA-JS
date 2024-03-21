98e988f97ed651710b27414f12fd62132a8cca3a
evellyn cibele <128157952+evellyncibele@users.noreply.github.com>
2024-03-21 08:39:11 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,9 @@
+idade = 17;
+
+if (idade < 15 && idade < 18 || idade > 70){
+    console.log("O voto é opicional");
+}else if (idade < 16){
+    console.log ("Você não pode votar");
+}else {
+    console.log ("Você é obrigado a votar");
+}
